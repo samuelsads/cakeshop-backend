4b88d4c3e2072fdb67c89729410662fc6c621cf2
Samuel Angel Dzib Sosa <samuelangeldzibsosa@MacBook-Pro-de-Samuel.local>
2023-10-19 22:02:38 -0600

    fix bug with payment

--- controllers/auth_payments.js
+++ controllers/auth_payments.js
@@ -1,67 +1,68 @@
-const {response} = require('express');
+const { response } = require('express');
 const Payments = require('../models/payments');
-const Orders  = require('../models/orders');
+const Orders = require('../models/orders');
 const mongoose = require('mongoose');
 
-const createPayment = async (req, res  = response)=>{
+const createPayment = async (req, res = response) => {
 
-    const {user_id} = req;
-    
-    req.body.user_id  = user_id;
+  const { user_id } = req;
 
-    
-    try {
-       
-          const orderDB  = await Orders.findById(req.body.order_id);
-        const totalPayments = await Payments.aggregate([
-            {
-              $match: { order_id: new mongoose.Types.ObjectId(req.body.order_id) }
-            },
-            {
-              $group: {
-                _id: null,
-                total: { $sum: '$payment' }
-              }
-            }
-          ]);
-        const  total = 0.0;
-          if (totalPayments.length > 0) {
-
-          if((totalPayments[0].total + payment.payment)  >orderDB.price){
-               return  res.json({success:true,  "msg":"El total de pagos supera el precio del producto"});
-
-            }
-            req.body.advance_payment = totalPayments[0].total;
-          } 
-      const payment  = new Payments(req.body);
-       await  payment.save(); 
+  req.body.user_id = user_id;
 
 
-       if((totalPayments[0].total + payment.payment) == orderDB.price){
-        orderDB.advance_payment = (totalPayments[0].total + payment.payment);
-        orderDB.paid = true;
-        orderDB.save();
+  try {
+
+    const orderDB = await Orders.findById(req.body.order_id);
+    const totalPayments = await Payments.aggregate([
+      {
+        $match: { order_id: new mongoose.Types.ObjectId(req.body.order_id) }
+      },
+      {
+        $group: {
+          _id: null,
+          total: { $sum: '$payment' }
+        }
       }
-    return res.json({success:true,  "msg":"Datos guardados correctamente"});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({success:false, msg:'Hable con el administrador'});
+    ]);
+    const total = 0.0;
+    if (totalPayments.length > 0) {
+
+      if ((totalPayments[0].total + req.body.payment) > orderDB.price) {
+        return res.json({ success: true, "msg": "El total de pagos supera el precio del producto" });
+
+      }
+      
+      req.body.advance_payment = totalPayments[0].total;
     }
-   
+    const payment = new Payments(req.body);
+    await payment.save();
+
+
+    if ((totalPayments[0].total + payment.payment) == orderDB.price) {
+      orderDB.advance_payment = (totalPayments[0].total + payment.payment);
+      orderDB.paid = true;
+      orderDB.save();
+    }
+    return res.json({ success: true, "msg": "Datos guardados correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, msg: 'Hable con el administrador' });
+  }
+
 }
 
 
-const allPaymentsByOrder = async(req, res  = response)=>{
-   const orderId = req.query.id;
-    try {
-       const clients=  await Payments.find({order_id:orderId}).populate('user_id').populate('order_id');
-        return res.json({success:true,   data: clients});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({success:false, msg:'Hable con el administrador'});
-    }
+const allPaymentsByOrder = async (req, res = response) => {
+  const orderId = req.query.id;
+  try {
+    const clients = await Payments.find({ order_id: orderId }).populate('user_id').populate('order_id');
+    return res.json({ success: true, data: clients });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, msg: 'Hable con el administrador' });
+  }
 
 
 }
 
-module.exports  = {createPayment,allPaymentsByOrder}
+module.exports = { createPayment, allPaymentsByOrder }
