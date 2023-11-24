2ce60ad208d28b4c6797b7a707d04a2f00401b11
Samuel Angel Dzib Sosa <samuelangeldzibsosa@MacBook-Pro-de-Samuel.local>
2023-11-23 22:01:16 -0600

    upd

--- controllers/auth_orders.js
+++ controllers/auth_orders.js
@@ -111,7 +111,7 @@ const allOrders = async (req, res = response) => {
             .populate('client_id', 'name father_surname mother_surname').populate('user_id', 'name father_surname mother_surname')
             .sort({ order_delivery_date: 1 })
             .skip(start).limit(limit);
-        return res.json({ success: true, data: orders });
+        return res.json({ success: true, data: orders, date:today });
     } catch (error) {
         console.log(error);
         res.status(500).json({ success: false, msg: 'Hable con el administradors' });
